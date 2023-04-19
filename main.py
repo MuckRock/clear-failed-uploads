bfc553bd354e6bd65088e68b930a13addd3f213d
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-04-19 11:38:50 -0500

    Adds comment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from documentcloud.addon import SoftTimeOutAddOn
 
 class Clear(SoftTimeOutAddOn):
-
+""" For all the documents, if the status of the document is error or nofile, deletes the document"""
     def main(self):
         for document in self.get_documents():
             if document.status == 'error' or document.status == 'nofile':
