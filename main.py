3131373124a709dde387a962a8f8076b25dccb4b
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2023-10-12 15:37:05 -0500

    Check to see if document was selected

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from documentcloud.addon import SoftTimeOutAddOn
 
 class Clear(SoftTimeOutAddOn):
     def main(self):
+        if not self.documents:
+            self.set_message("Please select at least one document.")
+            return
         for document in self.get_documents():
             if document.status == 'error' or document.status == 'nofile':
                 document.delete()
