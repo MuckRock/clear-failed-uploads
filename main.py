ec87b8fbf7beb9007e5fcbde3607d75f70eddafd
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2026-04-13 20:50:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import time
 from documentcloud.addon import SoftTimeOutAddOn
 
 class Clear(SoftTimeOutAddOn):
     def main(self):
+        self.client.session.headers.update({'User-Agent': 'Clear Failed Uploads Add-On'})
         for document in self.get_documents():
             if document.status == 'error' or document.status == 'nofile':
                 document.delete()
+                time.sleep(5)
  
 if __name__ == "__main__":
     Clear().main()
